9b2097b02976b75e62aa4892dc03484e27de2506
korchasa <korchasa@gmail.com>
2019-12-03 02:57:40 +0200

    feat: git username loading

--- command-new-issue.go
+++ command-new-issue.go
@@ -1,11 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+	"syscall"
+)
 
 type NewIssueCommand struct {
 }
 
 func (c *NewIssueCommand) Execute(args []string) error {
-	fmt.Println(args)
+	username, err := getUserFromGitConfig()
+	if err != nil {
+		logrus.Infof("can't get username from git config: %s", err)
+	}
+	fmt.Println(args, username)
+
 	return nil
+}
+
+func getUserFromGitConfig() (string, error) {
+	var stdout bytes.Buffer
+	cmd := exec.Command("git", []string{"config", "--get", "--null", "user.name"}...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = ioutil.Discard
+
+	err := cmd.Run()
+	if exitError, ok := err.(*exec.ExitError); ok {
+		if waitStatus, ok := exitError.Sys().(syscall.WaitStatus); ok {
+			if waitStatus.ExitStatus() == 1 {
+				return "", fmt.Errorf("git config not found")
+			}
+		}
+		return "", err
+	}
+
+	return strings.TrimRight(stdout.String(), "\000"), nil
 }
