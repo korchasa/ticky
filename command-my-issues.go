057cb55a72f88028496728c0df7f6e72c3ab1f06
korchasa <korchasa@gmail.com>
2019-12-05 03:19:06 +0200

    feat: ticky my

--- command-my-issues.go
+++ command-my-issues.go
@@ -1,8 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 type MyIssuesCommand struct {}
 
 func (c *MyIssuesCommand) Execute(_ []string) error {
+	user, err := getUserFromGitConfig()
+	if err != nil {
+		return err
+	}
+	iss, err := readAllIssues()
+	if err != nil {
+		return err
+	}
+
+	sortIssuesByStatusAndTitle(iss, Flags.Statuses)
+
+	fmt.Printf("Tickets assigned to @%s:\n", user)
+	for _, i := range iss {
+		for _, a := range i.Assignee {
+			if a == user {
+				fmt.Printf("  - %s [%s]\n", i.Title, i.Status)
+			}
+		}
+	}
+
+	fmt.Println()
 
 	return nil
+}
+
+func sortIssuesByStatusAndTitle(iss []Issue, statuses []string) {
+	indexOf := func(data []string, element string) int {
+		for k, v := range data {
+			if element == v {
+				return k
+			}
+		}
+		return -1
+	}
+	sort.Slice(iss, func(i, j int) bool {
+		if iss[i].Status != iss[j].Status {
+			return indexOf(statuses, iss[i].Status) < indexOf(statuses, iss[j].Status)
+		}
+		return iss[i].Title < iss[j].Title
+	})
 }
---
+++ command-my-issues_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_sortIssuesByStatusAndTitle(t *testing.T) {
+
+	{
+		byStatus := []Issue{{Status: "sa", Title: "a"}, {Status: "sb", Title: "z"}}
+		wantByStatus := []Issue{{Status: "sb", Title: "z"}, {Status: "sa", Title: "a"}}
+		sortIssuesByStatusAndTitle(byStatus, []string{"sb", "sa"})
+		assert.Equal(t, wantByStatus, byStatus)
+	}
+
+	{
+		byTitle := []Issue{{Title: "z"}, {Title: "a"}}
+		wantByTitle := []Issue{{Title: "a"}, {Title: "z"}}
+		sortIssuesByStatusAndTitle(byTitle, []string{"sb", "sa"})
+		assert.Equal(t, wantByTitle, byTitle)
+	}
+}
