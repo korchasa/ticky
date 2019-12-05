13348ea48cecc6f4ff945e7d00570bf955f17efd
korchasa <korchasa@gmail.com>
2019-12-06 00:30:36 +0200

    fix: info level instead of debug

--- main.go
+++ main.go
@@ -8,23 +8,22 @@ import (
 )
 
 var Flags struct {
-	Verbose bool `long:"verbose" short:"v" description:"Verbose output"`
-	Statuses []string
-	StatusesStr string `long:"statuses" short:"s" default:"todo,in-progress"`
-	IssuesDir string `long:"issues-dir" short:"i" description:"Issues directory" default:"issues"`
-	ReadmeCmd GenerateReadmeCommand `command:"readme" description:"Generate README.md"`
-	NewCmd NewIssueCommand `command:"new" description:"Create new issue"`
-	MyCmd MyIssuesCommand `command:"my" description:"Show my issues"`
+	Verbose     bool `long:"verbose" short:"v" description:"Verbose output"`
+	Statuses    []string
+	StatusesStr string                `long:"statuses" short:"s" default:"todo,in-progress"`
+	IssuesDir   string                `long:"issues-dir" short:"i" description:"Issues directory" default:"issues"`
+	ReadmeCmd   GenerateReadmeCommand `command:"readme" description:"Generate README.md"`
+	NewCmd      NewIssueCommand       `command:"new" description:"Create new issue"`
+	MyCmd       MyIssuesCommand       `command:"my" description:"Show my issues"`
 }
 
 func main() {
 	log.SetLevel(log.WarnLevel)
 	parser := flags.NewParser(&Flags, flags.Default)
-	parser.LongDescription ="Tiny issue tracking in source code"
 	parser.ShortDescription = "Tiny issue tracking in source code"
-	parser.CommandHandler = func (command flags.Commander, args []string) error {
+	parser.CommandHandler = func(command flags.Commander, args []string) error {
 		if Flags.Verbose {
-			log.SetLevel(log.DebugLevel)
+			log.SetLevel(log.InfoLevel)
 		}
 		for _, st := range strings.Split(Flags.StatusesStr, ",") {
 			Flags.Statuses = append(Flags.Statuses, strings.Trim(st, " "))
